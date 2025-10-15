539f665839dd536777e6792470f2c6cf21bbd571
Stefan Boos <kontakt@boos.systems>
2025-10-15 15:38:28 +0000

    feat: Mars Rover can move forward
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Stefan Boos <kontakt@boos.systems>

--- MarsRover.Tests/MarsRover.cs
+++ MarsRover.Tests/MarsRover.cs
@@ -3,8 +3,22 @@ namespace MarsRover.Tests;
 
 public class MarsRover
 {
-    public string input(string input)
+    private int[] Position { get; set; } = new int[2];
+    public MarsRover()
     {
-        return "0:0:N";
+        Position[0] = 0;
+        Position[1] = 0;
+    }
+
+    public string Input(string input)
+    {
+        foreach(var c in input)
+        {
+            if(c.ToString().ToUpperInvariant() == "M")
+            {
+                Position[1]++;
+            }
+        }
+        return $"{Position[0]}:{Position[1]}:N";
     }
 }
--- MarsRover.Tests/MarsRoverTest.cs
+++ MarsRover.Tests/MarsRoverTest.cs
@@ -4,13 +4,35 @@ namespace MarsRover.Tests;
 public class MarsRoverTest
 {
     [Fact]
-    public void Test1()
+    public void GivenEmptyInput_RoverReturnsStartingPosition()
     {
         var input = "";
         var expected = "0:0:N";
 
         var rover = new MarsRover();
-        var actual = rover.input(input);
+        var actual = rover.Input(input);
+
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Given1MoveCommand_RoverMoves1North()
+    {
+        var input = "M";
+        var expected = "0:1:N";
+
+        var rover = new MarsRover();
+        var actual = rover.Input(input);
+
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Given9MoveCommands_RoverMoves9North()
+    {
+        var input = "MMMMMMMMM";
+        var expected = "0:9:N";
+
+        var rover = new MarsRover();
+        var actual = rover.Input(input);
 
         Assert.Equal(expected, actual);
     }
