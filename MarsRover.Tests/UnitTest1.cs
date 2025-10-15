f2bf0445dc2c8eed638cf6d24b2ca8a0250ed5c9
Stefan Boos <kontakt@boos.systems>
2025-10-15 15:12:30 +0000

    feat: MarsRover starts at 0:0:N
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Stefan Boos <kontakt@boos.systems>

---
+++ MarsRover.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MarsRover.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var input = "";
+        var expected = "0:0:N";
+
+        var rover = new MarsRover();
+        var actual = rover.input(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
+
+public class MarsRover
+{
+    public string input(string input)
+    {
+        return "0:0:N";
+    }
+}
